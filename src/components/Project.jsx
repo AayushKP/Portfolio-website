8a207a2cb35e4cd53104f3a414eb510a7963f05b
Ayush Kashyap <ayushprokashyap@gmail.com>
2025-08-27 17:16:18 +0530

    updated link

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -119,7 +119,7 @@ function ProjectsGrid() {
         "Tailwind",
         "Cloudinary",
       ],
-      live: "https://resonance-beta.vercel.app",
+      live: "https://resonance-hitk.com",
       github: "https://github.com/AayushKP/Resonance",
     },
     {
